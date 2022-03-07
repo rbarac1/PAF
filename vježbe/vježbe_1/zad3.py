4f16bffbac5569321258b209f3ce60c1df63d5b1
rbarac1 <rbarac@pmfst.hr>
2022-03-07 11:36:32 +0100

    3. zadatak

--- vježbe/vježbe_1/zad3.py
+++ vježbe/vježbe_1/zad3.py
@@ -1,9 +1,10 @@
+#unos i provjera koordinata prve točke
 i=1
 while i:
     try:
         x1,y1 = input('Unesite koordinate prve točke u formatu x,y: \t').split(',')
-        float(x1)
-        float(y1)
+        x1 = float(x1)
+        y1 = float(y1)
     except ValueError:
         print("Ponovite upis")
     except:
@@ -12,15 +13,26 @@ while i:
         i = 0
 
 
+#unos i provjera koordinata druge točke
 i=1
 while i:
     try:
         x2,y2 = input('Unesite koordinate druge točke u formatu x,y: \t').split(',')
-        float(x2)
-        float(y2)
+        x2 = float(x2)
+        y2 = float(y2)
     except ValueError:
         print("Ponovite upis")
     except:
         print("Ponovite upis")
     else:
         i = 0
+
+
+#nagib pravca
+a = (y2-y1)/(x2-x1)
+
+#pomak na y osi
+b = y1 - a*x1
+
+#printanje jednadbžbe pravca, za veću preciznost mijenjajte preciznost broj znamenki i decimala u floatu
+print("Jednažba pravca je y = {:5.4f} x + {:5.4f}".format(a,b))
