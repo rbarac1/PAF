0c635046c73888b5402287759526659370ffce17
rbarac1 <rbarac@pmfst.hr>
2022-05-08 21:49:32 +0200

    vježbe 7

--- vježbe/vježbe_7/zad.cpp
+++ vježbe/vježbe_7/zad.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main(){
-    Particle p1(100,45,10,0);
+    Particle p1(100,45,0,0);
     cout << "Domet čestice 1 je: ";
     p1.range();
     cout << "Vrijeme leta čestice 1 je: ";
